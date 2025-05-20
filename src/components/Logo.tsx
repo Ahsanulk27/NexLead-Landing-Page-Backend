45d40e5b850eb07e58acc1b64f6a71cc06b8fe5f
Ahsanul <ahsanulkarim2002@gmail.com>
2025-05-20 22:18:35 +0600

    overall color scheme changed

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -5,7 +5,7 @@ const Logo: React.FC = () => {
     <a href="#" className="text-2xl font-bold text-text-light">
       <div className="w-28 h-10 overflow-hidden">
         <img
-          src="public/images/logo.png"
+          src="/images/logo.png"
           alt="logo"
           className="w-full h-full object-cover scale-[2.6] object-center"
         />
